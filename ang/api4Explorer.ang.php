40a6e65de144ae97015c3f1a6646a3f5038d4a4b
Coleman Watts <coleman@civicrm.org>
2019-09-16 16:31:24 -0400

    Fix api explorer module loading

--- ang/api4Explorer.ang.php
+++ ang/api4Explorer.ang.php
@@ -1,14 +1,13 @@
 <?php
 // Autoloader data for Api4 explorer.
 return [
+  'ext' => 'civicrm',
   'js' => [
     'ang/api4Explorer.js',
-    'ang/api4Explorer/*.js',
-    'ang/api4Explorer/*/*.js',
-    'lib/*.js',
+    'ang/api4Explorer/Explorer.js',
   ],
   'css' => [
-    'css/explorer.css',
+    'css/api4-explorer.css',
   ],
   'partials' => [
     'ang/api4Explorer',
