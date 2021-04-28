88cc3b95b1298eace8ec063c819a332baeb403ca
Tim Otten <totten@civicrm.org>
2021-04-27 23:03:46 -0700

    dev/translation#67 - Add APIv4 support for "Strings" entity

---
+++ Civi/Api4/Strings.php
@@ -0,0 +1,19 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * Store supplemental translations for strings on DB entitiesi.
+ *
+ * @package Civi\Api4
+ */
+class Strings extends Generic\DAOEntity {
+
+  public static function permissions() {
+    return [
+      // If we add support for DynamicFKAuthorization, then these might be relaxed. As is, 'translate CiviCRM' is interpreted to mean "translate anything and everything in CiviCRM".
+      'meta' => ['access CiviCRM'],
+      'default' => ['translate CiviCRM'],
+    ];
+  }
+
+}
---
+++ tests/phpunit/api/v4/Entity/StringsTest.php
@@ -0,0 +1,194 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
+
+namespace api\v4\Entity;
+
+use api\v4\UnitTestCase;
+
+/**
+ * @group headless
+ */
+class StringsTest extends UnitTestCase {
+
+  protected $ids = [];
+
+  public function getCreateOKExamples() {
+    $es = [];
+
+    $es['asDraft'] = [
+      [
+        'status_id:name' => 'draft',
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'description',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => 'Hello world',
+      ],
+    ];
+
+    $es['defaultStatus'] = [
+      [
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'title',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => 'Hello title',
+      ],
+    ];
+
+    return $es;
+  }
+
+  public function getCreateBadExamples() {
+    $es = [];
+
+    $es['badStatus'] = [
+      [
+        'status_id:name' => 'jumping',
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'description',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => 'Hello world',
+      ],
+      '/Invalid status/',
+    ];
+
+    $es['malformedField'] = [
+      [
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'ti!tle',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => 'Hello title',
+      ],
+      '/Entity reference is malformed/',
+    ];
+
+    $es['badTable'] = [
+      [
+        'entity_table' => 'typozcivicrm_event',
+        'entity_field' => 'title',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => 'Hello title',
+      ],
+      '/non-existent or non-translatable table/',
+    ];
+
+    $es['badFieldName'] = [
+      [
+        'status_id:name' => 'active',
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'zoological_taxonomy',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => 'Hello world',
+      ],
+      '/non-existent or non-translatable field/',
+    ];
+
+    $es['badFieldType'] = [
+      [
+        'status_id:name' => 'active',
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'event_type_id',
+        'entity_id' => '*EVENT*',
+        'language' => 'fr_CA',
+        'string' => '9',
+      ],
+      '/non-existent or non-translatable field/',
+    ];
+
+    $es['badEntityId'] = [
+      [
+        'status_id:name' => 'active',
+        'entity_table' => 'civicrm_event',
+        'entity_field' => 'description',
+        'entity_id' => 9999999,
+        'language' => 'fr_CA',
+        'string' => 'Hello world',
+      ],
+      '/Entity does not exist/',
+    ];
+
+    return $es;
+  }
+
+  protected function setUp() {
+    parent::setUp();
+    $this->ids = [];
+  }
+
+  /**
+   * @dataProvider getCreateOKExamples
+   * @param array $record
+   */
+  public function testCreateOK($record) {
+    $record = $this->fillRecord($record);
+    $createResults = \civicrm_api4('Strings', 'create', [
+      'checkPermissions' => FALSE,
+      'values' => $record,
+    ]);
+    $this->assertEquals(1, $createResults->count());
+    foreach ($createResults as $createResult) {
+      $getResult = \civicrm_api4('Strings', 'get', [
+        'where' => [['id', '=', $createResult['id']]],
+      ]);
+      $this->assertEquals($record['string'], $getResult->single()['string']);
+    }
+  }
+
+  ///**
+  // * @dataProvider getCreateBadExamples
+  // * @param array $record
+  // * @param string $errorRegex
+  // *   Regular expression to compare against the error message.
+  // */
+  // FIXME: validation
+  //public function testCreateBad($record, $errorRegex) {
+  //  $record = $this->fillRecord($record);
+  //  try {
+  //    \civicrm_api4('Strings', 'create', [
+  //      'checkPermissions' => FALSE,
+  //      'values' => $record,
+  //    ]);
+  //    $this->fail('Create should have failed');
+  //  }
+  //  catch (\API_Exception $e) {
+  //    $this->assertRegExp($errorRegex, $e->getMessage());
+  //  }
+  //}
+
+  /**
+   * Fill in mocked values for the would-be record..
+   *
+   * @param array $record
+   *
+   * @return array
+   */
+  protected function fillRecord($record) {
+    if ($record['entity_id'] === '*EVENT*') {
+      $eventId = $this->ids['*EVENT*'] ?? \CRM_Core_DAO::createTestObject('CRM_Event_BAO_Event')->id;
+      $record['entity_id'] = $this->ids['*EVENT*'] = $eventId;
+    }
+    return $record;
+  }
+
+}
