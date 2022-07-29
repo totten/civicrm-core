8542d07c7e8b1cbb65ad878373eb344f572c70f3
Tim Otten <totten@civicrm.org>
2022-07-29 00:39:00 -0700

    AfformInlay - Print debug message with list of needed resources

--- ext/afform/core/Civi/Afform/AfformInlay.php
+++ ext/afform/core/Civi/Afform/AfformInlay.php
@@ -2,6 +2,7 @@
 
 namespace Civi\Afform;
 
+use Civi\Angular\AngularLoader;
 use Civi\Inlay\ApiRequest;
 
 class AfformInlay extends \Civi\Inlay\Type {
@@ -23,7 +24,33 @@ class AfformInlay extends \Civi\Inlay\Type {
   }
 
   public function getExternalScript(): string {
-    return '';
+    $result = '';
+
+    $writeln = function($msg, ...$args) use (&$result) {
+      $result .= sprintf("document.write(%s);\n", json_encode(
+        sprintf($msg, ...$args) . "<br/>\n"
+      ));
+    };
+
+    $writeln('TODO: Render form "%s"', $this->config['formName']);
+
+    $regionName = 'af-inlay-' . $this->config['formName'];
+    $region = \CRM_Core_Region::instance($regionName);
+
+    $loader = new AngularLoader();
+    $loader->setRegion($regionName);
+    $loader->setPageName($this->config['formName'] . '/inlay');
+    $loader->addModules($this->config['formName']);
+    // $loader->useApp();
+    \Civi::dispatcher()->addListener('civi.region.render', [$loader, 'onRegionRender']);
+
+    $region->render('', FALSE);
+    foreach ($region->getAll() as $snippet) {
+      $writeln("TODO: Load resource \"%s\" (%s)", $snippet['name'], $snippet['type']);
+    }
+
+    $region->clear();
+    return $result;
   }
 
 }
