9898db925e19ca9ee80efdebfed2689c00333d20
Tim Otten <totten@civicrm.org>
2023-02-10 18:42:18 -0800

    (Untested) Blind update of "ext/" to 23.01.0.up.php

--- ext/greenwich/greenwich.civix.php
+++ ext/greenwich/greenwich.civix.php
@@ -92,9 +92,6 @@ function _greenwich_civix_civicrm_config($config = NULL) {
   $configured = TRUE;
 
   $extRoot = __DIR__ . DIRECTORY_SEPARATOR;
-  $extDir = $extRoot . 'templates';
-  CRM_Core_Smarty::singleton()->addTemplateDir($extDir);
-
   $include_path = $extRoot . PATH_SEPARATOR . get_include_path();
   set_include_path($include_path);
   // Based on <compatibility>, this does not currently require mixin/polyfill.php.
