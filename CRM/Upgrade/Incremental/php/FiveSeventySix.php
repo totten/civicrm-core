bf44417ea2eb013d50e4c4b00d020dec01660850
Allen Shaw <allen@JoineryHQ.com>
2024-06-16 14:14:02 -0500

    Toward dev/core#3682: Add upgrader task to create the message_header CustomToken

--- CRM/Upgrade/Incremental/php/FiveSeventySix.php
+++ CRM/Upgrade/Incremental/php/FiveSeventySix.php
@@ -30,6 +30,19 @@ class CRM_Upgrade_Incremental_php_FiveSeventySix extends CRM_Upgrade_Incremental
   public function upgrade_5_76_alpha1($rev): void {
     $this->addTask(ts('Upgrade DB to %1: SQL', [1 => $rev]), 'runSql', $rev);
     $this->addTask('Install CustomToken entity', 'createEntityTable', '5.76.alpha1.CustomToken.entityType.php');
+    $this->addTask('Create "message header" token', 'create_mesage_header_token');
   }
 
+  public static function create_mesage_header_token() {
+    $token = Civi\Api4\CustomToken::create(FALSE)
+      ->addValue('name', 'message_header')
+      ->addValue('label', 'Message Heaader')
+      ->addValue('body_html', '<tr></tr>')
+      ->addValue('body_text', '')
+      ->addValue('is_reserved', TRUE)
+      ->addValue('is_active', TRUE)
+      ->execute()
+      ->first();
+    return (!empty($token['id']));
+  }
 }
