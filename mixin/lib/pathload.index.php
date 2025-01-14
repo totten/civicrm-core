72626b7c3fd43020e0bf978e41cba0446c935d3d
Tim Otten <totten@civicrm.org>
2025-01-13 23:11:28 -0800

    civimix-schema - Split off major-version from core. Keep 5.x.
    Before:
    
    * 'mixin/lib/civimix-schema' is numbered `5.x.x`
    * `mixin/lib/civimix-schema` is strictly pegged to the 'civicrm-core'
    
    After:
    
    * `mixin/lib/civimix-schema@5` is numbered `5.x.x.`
    * `mixin/lib/civimix-schema@5` is partially released from the numbering of `civicrm-core`.
    
    Comments:
    
    The version will still increment automatically (month-to-month; in parallel
    with core), but it will stay within 5.x. This is because it's supposed to be
    strictly SemVer, and we haven't actually made any major changes to it, so
    moving to 6.x would be a big hassle.

--- mixin/lib/pathload.index.php
+++ mixin/lib/pathload.index.php
@@ -5,11 +5,18 @@ namespace Civi\PathLoadSetup;
 //
 //   pathload()->addSearchDir(__DIR__ . '/lib');
 //
-// However, that would detect version#'s from the filenames. In this folder,
-// we want all subprojects to have the same version-number as the main
-// project. It would be quite inconvenient to rename them every month.
+// However, that would detect version#'s from the filenames -- which is a convenient
+// practice for downstreams using backports. However, here, we are canonical.
+// It would be quite inconvenient to rename the folder every time there's an edit.
 //
-// So instead, we use `addSearchItem()` and register with explicit versions.
+// `addSearchItem()` allows us to register version-number programmatically -- so we don't have
+// to manually set number.
 
-$version = \CRM_Utils_System::version() . '.1'; /* Higher priority than contrib copies of same version... */
-\pathload()->addSearchItem('civimix-schema', $version, __DIR__ . '/civimix-schema');
+$version6 = \CRM_Utils_System::version() . '.1'; /* Higher priority than contrib copies of same version... */
+$version5 = preg_replace_callback(';^6\.(\d+)\.;', function ($m) {
+  /* civimix-schema@5.83=>6.0 was purely superficial (to match core#)s. Continue 5.x option for compat. */
+  return '5.' . (83 + $m[1]) . '.';
+}, $version6);
+
+// Register civimix-schema@5.x
+\pathload()->addSearchItem('civimix-schema@5', $version5, __DIR__ . '/civimix-schema');
