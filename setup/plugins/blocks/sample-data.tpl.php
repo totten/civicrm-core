f53b378f69bbf12731efdebc12aaef076d3193b1
Tim Otten <totten@civicrm.org>
2025-04-28 12:57:42 -0700

    sample-data - Allow choosing sample data with other languages.

--- setup/plugins/blocks/sample-data.tpl.php
+++ setup/plugins/blocks/sample-data.tpl.php
@@ -6,18 +6,3 @@ endif; ?>
   <label for="loadGenerated"><span><?php echo ts('Fill system with fake information'); ?></span> <input id="loadGenerated" type="checkbox" name="civisetup[loadGenerated]" value=1 <?php echo $model->loadGenerated ? "checked='checked'" : ""; ?> /></label> <br />
   <span class="advancedTip"><?php echo ts("To initialize a demo site, load sample data about fake people. Generated in English (United States)."); ?></span><br />
 </p>
-
-<script type="text/javascript">
-  csj$(function($) {
-    function hideLang() {
-      if ($('[name=lang]').val() === 'en_US') {
-        $('#loadGenerated').prop('disabled', false);
-      }
-      else {
-        $('#loadGenerated').prop('disabled', true).prop('checked', false);
-      }
-      setTimeout(hideLang, 100);
-    }
-    setTimeout(hideLang, 100);
-  });
-</script>
